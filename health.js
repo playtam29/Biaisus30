3c6ccacb26dc13120a3510c3372c0b56f0bd71ec
playtam29 <117916566+playtam29@users.noreply.github.com>
2026-04-28 03:14:25 +0200

    Update health.js

--- health.js
+++ health.js
@@ -1,5 +1,1 @@
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { 'Content-Type': 'application/json', 'Cache-Control': 'no-store' },
-  body: JSON.stringify({ ok: true, function: 'health', version: 'v3' })
-});
+
